81ec210f84c0a562657ad4ffaddc2b524c8cf462
pincopallino <pincopallino@33a41c02-bd0a-4875-89b2-fde9505b2c38>
2012-01-30 07:54:11 +0000

    PDF icon; Fixed crash when deleting nonexistent log file

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -88,6 +88,13 @@ Namespace My.Resources
             End Get
         End Property
         
+        Friend ReadOnly Property pdf_icon() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("pdf_icon", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
         Friend ReadOnly Property printer() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("printer", resourceCulture)
