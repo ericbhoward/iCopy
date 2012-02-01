5ad398adae4e5cf4fc5b2ef5f9f5f63d56d9230c
pincopallino <pincopallino@33a41c02-bd0a-4875-89b2-fde9505b2c38>
2012-02-01 15:52:00 +0000

    Added example log from a Brother scanner

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.5.0.49")> 
-<Assembly: AssemblyFileVersion("1.5.0.49")> 
+<Assembly: AssemblyVersion("1.5.0.50")> 
+<Assembly: AssemblyFileVersion("1.5.0.50")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")> 
